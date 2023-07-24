66ac1267260562fa2658a9437e0c32548b0422e8
Alena Marhayova <amarhayova@exadel.com>
2023-07-24 14:57:02 +0200

    sorting with localization and using Set as result

--- src/main/java/collections/Main.java
+++ src/main/java/collections/Main.java
@@ -1,10 +1,12 @@
 /*
 There are 2 collections with id and title properties.
-Task: find items that are present in both collections without duplicates and sort them by title.
+Task: find items that are present in both collections without duplicates and
+sort them by title taking in account localization.
 */
 
 package collections;
 
+import java.text.Collator;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -16,29 +18,34 @@ public class Main {
                 new Item(6, "F"),
                 new Item(1, "A"),
                 new Item(1, "A"),
+                new Item(27, "Ä"),
                 new Item(2, "B"),
                 new Item(3, "C"));
         List<Item> items2 = new ArrayList<>();
         Collections.addAll(items2,
                 new Item(4, "D"),
                 new Item(7, "G"),
-                new Item(1, "A"),
+                new Item(28, "Ü"),
                 new Item(5, "E"),
+                new Item(27, "Ä"),
                 new Item(3, "C"),
-                new Item(3, "C"));
-        List<Item> uniqueItems = items1.stream()
-                .distinct()
-                .filter(items2::contains)
-                .collect(Collectors.toList());
-        uniqueItems.sort((o1, o2) -> {
-            if (o1 instanceof Item && o2 instanceof Item) {
-                Item item1 = o1;
-                Item item2 = o2;
-                return item1.getTitle().compareTo(item2.getTitle());
-            }
-            return 0;
-        });
-        for (Item item : uniqueItems) {
+                new Item(3, "C"),
+                new Item(1, "A"));
+        Set<Item> uniqueItems = getUniqueItemsSortedSet(items1,items2, Locale.GERMAN);
+        printCollection(uniqueItems);
+    }
+
+    private static Set<Item> getUniqueItemsSortedSet(List<Item> list1, List<Item> list2, Locale locale) {
+        Collator localeCollector = Collator.getInstance(locale);
+        Set<Item> uniqueItems = list1.stream()
+                .filter(list2::contains)
+                .sorted(Comparator.comparing(Item::getTitle,localeCollector))
+                .collect(Collectors.toSet());
+        return uniqueItems;
+    }
+
+    private static void printCollection(Collection<Item> items){
+        for (Item item : items) {
             System.out.println(item);
         }
     }
